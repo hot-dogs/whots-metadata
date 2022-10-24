f09d4230dd1b3440d9c6ddd0eddc3c4688d83d3c
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-10-24 07:07:02 -1000

    created setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Build and install the project.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='whots-metadata',
+    version='0.0.1',
+    extras_require={
+        'scrapy': ['scrapy>=2.7.0'],
+    },
+    packages=find_packages(where='whots_metadata'),
+    package_dir={"": "whots_metadata"},
+    author='Fernando Carvalho Pacheco',
+    author_email='fernando.pacheco@hawaii.edu',
+    description='A scrapy framework project for scraping WHOTS information  '
+                'from https://uop.whoi.edu/currentprojects/WHOTS/whotsdata.html',
+)
+
