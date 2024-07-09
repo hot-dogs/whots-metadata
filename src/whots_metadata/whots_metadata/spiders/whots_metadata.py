fa3d2b4425bcf313fbc36c2da062884174890775
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2024-07-09 09:37:12 -1000

    Updated/fixed previous buoy spider. Had to change whots_syst1 and 2 from a_nth-child:10 and 11 to 9 and 10

--- src/whots_metadata/whots_metadata/spiders/whots_metadata.py
+++ src/whots_metadata/whots_metadata/spiders/whots_metadata.py
@@ -1,4 +1,5 @@
 import scrapy
+
 from ..items import WhotsMetadataItem
 
 URL = "https://uop.whoi.edu/currentprojects/WHOTS/whotsdata.html"
@@ -9,19 +10,17 @@ BUOY_CSS = {
         "WHOTS_SYS1": {"a_nth-child": 9},
         "WHOTS_SYS2": {"a_nth-child": 10},
     },
-
     "PREVIOUS": {
         "WHOTS_NUMBER": {"tr_nth-child": 3},
-        "WHOTS_SYS1": {"a_nth-child": 10},
-        "WHOTS_SYS2": {"a_nth-child": 11},
+        "WHOTS_SYS1": {"a_nth-child": 9 },
+        "WHOTS_SYS2": {"a_nth-child": 10},
     }
 }
 
 SELECTOR = 'div#content3 > div > div:nth-child(4) > table:nth-child(1)'
 TR_SELECTOR = " tr:nth-child("
 
-
-class WhotsScrapy(scrapy.Spider):
+class WhotsMetadataSpider(scrapy.Spider):
     name = 'whotsmetadata'
     start_urls = [
         URL
@@ -30,50 +29,78 @@ class WhotsScrapy(scrapy.Spider):
     def parse(self, response):
         items = WhotsMetadataItem()
         main_selector = response.css(SELECTOR)
+        
+        if not main_selector:
+            self.log("Main selector is None", level=scrapy.log.ERROR)
+            return
+        
         for keys, values in BUOY_CSS.items():
-            number = main_selector.css(
+            number_selector = main_selector.css(
                 TR_SELECTOR +
                 str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
                 ")" +
-                "> td > strong:nth-child(1)::text").extract_first()[-2:]
+                "> td > strong:nth-child(1)::text"
+            )
+            number = number_selector.extract_first()
+            if number:
+                number = number[-2:]
+            self.log(f"Number for {keys}: {number}")
 
-            sys1 = main_selector.css(
-                TR_SELECTOR +
-                str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
-                ")" +
-                "> td > a:nth-child(" +
-                str(values["WHOTS_SYS1"]["a_nth-child"]) +
-                ")::text").extract_first()
-
-            sys2 = main_selector.css(
-                TR_SELECTOR +
-                str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
-                ")" +
-                "> td > a:nth-child(" +
-                str(values["WHOTS_SYS2"]["a_nth-child"]) +
-                ")::text").extract_first()
-
-            link1 = main_selector.css(
+            sys1_selector = main_selector.css(
                 TR_SELECTOR +
                 str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
                 ")" +
                 "> td > a:nth-child(" +
                 str(values["WHOTS_SYS1"]["a_nth-child"]) +
-                ")::attr(href)").extract_first()[:-6] + '.txt'
+                ")::text"
+            )
+            sys1 = sys1_selector.extract_first()
+            self.log(f"Sys1 for {keys}: {sys1}")
 
-            link2 = main_selector.css(
+            sys2_selector = main_selector.css(
                 TR_SELECTOR +
                 str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
                 ")" +
                 "> td > a:nth-child(" +
                 str(values["WHOTS_SYS2"]["a_nth-child"]) +
-                ")::attr(href)").extract_first()[:-6] + '.txt'
+                ")::text"
+            )
+            sys2 = sys2_selector.extract_first()
+            self.log(f"Sys2 for {keys}: {sys2}")
 
-            items["number"] = number
-            items["sys1"] = sys1
-            items["sys2"] = sys2
-            items["link1"] = (URL[:-14] + link1)
-            items["link2"] = (URL[:-14] + link2)
+            link1_selector = main_selector.css(
+                TR_SELECTOR +
+                str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
+                ")" +
+                "> td > a:nth-child(" +
+                str(values["WHOTS_SYS1"]["a_nth-child"]) +
+                ")::attr(href)"
+            )
+            link1 = link1_selector.extract_first()
+            if link1:
+                link1 = link1[:-6] + '.txt'
+            self.log(f"Link1 for {keys}: {link1}")
 
-            yield {keys: items}
+            link2_selector = main_selector.css(
+                TR_SELECTOR +
+                str(values["WHOTS_NUMBER"]["tr_nth-child"]) +
+                ")" +
+                "> td > a:nth-child(" +
+                str(values["WHOTS_SYS2"]["a_nth-child"]) +
+                ")::attr(href)"
+            )
+            link2 = link2_selector.extract_first()
+            if link2:
+                link2 = link2[:-6] + '.txt'
+            self.log(f"Link2 for {keys}: {link2}")
+
+            if number and sys1 and sys2 and link1 and link2:
+                items["number"] = number
+                items["sys1"] = sys1
+                items["sys2"] = sys2
+                items["link1"] = (URL[:-14] + link1)
+                items["link2"] = (URL[:-14] + link2)
+                yield {keys: items}
+            else:
+                self.log(f"Skipping {keys} due to missing data", level=scrapy.log.WARNING)
 
